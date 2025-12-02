19d9fce93298196bd0ac976afcf99476d2ca1139
Mahicm <mahichinu02@gmail.com>
2025-12-03 00:19:01 +0530

    Added Resume

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -36,8 +36,8 @@ const Hero = () => {
                 <ArrowRight className="ml-2 w-4 h-4 group-hover:translate-x-1 transition-transform" />
               </a>
               <a 
-                 href={`https://mail.google.com/mail/?view=cm&to=${PERSONAL_INFO.email}`}
-                target="_blank"
+                 href="/public/resume/Mahindha_Resume.pdf"
+                download="Mahindha_Resume.pdf"
                 rel="noopener noreferrer"
                 className="inline-flex items-center justify-center px-8 py-3.5 text-sm font-medium text-white bg-zinc-900   rounded-lg hover:bg-zinc-800 transition-all group"
               >
